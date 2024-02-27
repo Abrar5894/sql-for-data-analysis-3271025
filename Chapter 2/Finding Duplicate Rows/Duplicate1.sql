90e18bb53dd72e14b3130f9ffc8af0b50cba9c9a
Abrar5894 <57849621+Abrar5894@users.noreply.github.com>
2024-02-27 20:44:59 +0000

    Rearranged last and first name

--- Chapter 2/Finding Duplicate Rows/Duplicate1.sql
+++ Chapter 2/Finding Duplicate Rows/Duplicate1.sql
@@ -1,10 +1,10 @@
 SELECT Customer.CustomerID,
-  Customer.FirstName,
   Customer.LastName,
+  Customer.FirstName,
   COUNT(1) as NUM_ORDERS
 FROM Customer
   JOIN Orders ON Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
-  Customer.FirstName,
-  Customer.LastName
+  Customer.LastName,
+  Customer.FirstName
 ORDER BY NUM_ORDERS DESC
